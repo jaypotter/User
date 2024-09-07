d90c73be33526012367ef27d2845cc9e5b1c4644
Jay Potter <j@ypotter.ca>
2024-09-07 17:30:18 -0400

    Create AbstractSession.php

---
+++ src/Potter/User/Session/AbstractSession.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\User\Session;
+
+use Potter\Database\Table\Aware\AbstractTableAware;
+
+abstract class AbstractSession extends AbstractTableAware implements SessionInterface
+{
+    abstract public function getSessionId(): string;
+    abstract protected function setSessionId(string $sessionId): string;
+    abstract public function getSessionName(): string;
+    abstract protected function setSessionName(string $sessionName): string;
+    abstract public function startSession(): void;
+}
