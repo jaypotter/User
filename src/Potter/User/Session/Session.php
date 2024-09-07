62a987a8a5b1a7890c48ea1285172827d18ff144
Jay Potter <j@ypotter.ca>
2024-09-07 17:47:08 -0400

    Update Session.php

--- src/Potter/User/Session/Session.php
+++ src/Potter/User/Session/Session.php
@@ -4,21 +4,23 @@ declare(strict_types=1);
 
 namespace Potter\User\Session;
 
-use Potter\{
-    Aware\AwareTrait,
-    Database\Table\Aware\TableAwareTrait
+use Potter\Aware\AwareTrait;
+use Potter\Database\Table\{
+    TableInterface,
+    Aware\TableAwareTrait
 };
 
 final class Session extends AbstractSession
 {
     use AwareTrait, SessionTrait, TableAwareTrait;
     
-    public function __construct(?string $sessionName = null)
+    public function __construct(?string $sessionName = null, ?TableInterface $table = null)
     {
         if (!is_null($sessionName)) {
             $this->setSessionName($sessionName);
         }
         $this->startSession();
+        $this->setTable($table);
         if (!$this->hasTable()) {
             return;
         }
