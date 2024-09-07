a71c19ee8e47364327323eacfd2122600ef785f9
Jay Potter <j@ypotter.ca>
2024-09-07 17:30:23 -0400

    Create Session.php

---
+++ src/Potter/User/Session/Session.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\User\Session;
+
+use Potter\{
+    Aware\AwareTrait,
+    Database\Table\Aware\TableAwareTrait
+};
+
+final class Session extends AbstractSession
+{
+    use AwareTrait, SessionTrait, TableAwareTrait;
+    
+    public function __construct(?string $sessionName = null)
+    {
+        if (!is_null($name)) {
+            $this->setSessionName($sessionName);
+        }
+    }
+}
