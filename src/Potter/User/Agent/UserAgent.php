dffd186c66971aa35f28a821a949aa5a6006d5c8
Jay Potter <j@ypotter.ca>
2024-09-08 21:29:18 -0400

    Create UserAgent.php

---
+++ src/Potter/User/Agent/UserAgent.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\User\Agent;
+
+use Potter\Aware\AwareTrait;
+use Potter\Database\Table\Aware\TableAwareTrait;
+use Potter\Database\Table\TableInterface;
+
+final class UserAgent extends AbstractUserAgent
+{
+    use AwareTrait, TableAwareTrait, UserAgentTrait;
+    
+    public function __construct(?TableInterface $table = null)
+    {
+        $this->setTable($table);
+    }
+}
