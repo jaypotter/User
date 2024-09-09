fde45da815da898c627e76f99c3b5ec63c56dd67
Jay Potter <j@ypotter.ca>
2024-09-08 21:29:10 -0400

    Create UserAgentInterface.php

---
+++ src/Potter/User/Agent/UserAgentInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\User\Agent;
+
+use Potter\Database\Table\Aware\TableAwareInterface;
+
+interface UserAgentInterface extends TableAwareInterface
+{
+    public function createTableIfNotExists(): void;
+    public function getServerAPI(): string;
+    public function getUserAgent(): string;
+    public function initiate(): void;
+    public function isConsoleUser(): bool;
+}
