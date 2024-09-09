ae3751987ecaf7e1ee5808c8272e64614b7490eb
Jay Potter <j@ypotter.ca>
2024-09-08 21:29:13 -0400

    Create AbstractUserAgent.php

---
+++ src/Potter/User/Agent/AbstractUserAgent.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\User\Agent;
+
+abstract class AbstractUserAgent implements UserAgentInterface
+{
+    abstract public function createTableIfNotExists(): void;
+    abstract public function getServerAPI(): string;
+    abstract public function getUserAgent(): string;
+    abstract public function initiate(): void;
+    abstract public function isConsoleUser(): bool;
+}
